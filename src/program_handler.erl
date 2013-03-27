d5bb411fba2ac0e0ea002adbc57ec880cfbf034a
Andrey Pavlov <couchemar@yandex.ru>
2013-03-27 10:29:45 +0400

    Увеличили время кэширования

--- src/program_handler.erl
+++ src/program_handler.erl
@@ -70,4 +70,4 @@ add_cors_headers(Req) ->
     Req2 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req1),
     Req3 = cowboy_req:set_resp_header(<<"access-control-allow-headers">>,
                                       <<"content-type, accept, x-requested-with, origin">>, Req2),
-    cowboy_req:set_resp_header(<<"access-control-max-age">>, <<"10">>, Req3).
+    cowboy_req:set_resp_header(<<"access-control-max-age">>, <<"600">>, Req3).
