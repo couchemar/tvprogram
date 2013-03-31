bf218cbd9614c8236d14531a6e8750f0837e7914
Andrey Pavlov <couchemar@yandex.ru>
2013-03-31 22:58:16 +0400

    lJGBCFKB

--- src/channel_handler.erl
+++ src/channel_handler.erl
@@ -35,17 +35,24 @@ content_types_provided(Req, State) ->
 channels(Req, State) ->
     Host = {localhost, 27017},
     {ok, Conn} = mongo:connect(Host),
-    {ok, Cursor} = mongo:do(
-                     safe, master, Conn, tv,
-                     mongo:find(
-                       channels,
-                       {}, {}
-                      )
-                    ),
-    Result = 1,
+    {ok, Cursor} = mongo:do(safe, master, Conn, tv,
+                            mongo:find(channels, {}, {})),
+    Result = process(Cursor),
     Json = jsx:encode([{<<"channels">>, Result}]),
     {Json, rest_utils:add_cors_headers(Req), State}.
 
 %% =======
 %% Private
 %% =======
+
+process(Cursor) ->
+    process(Cursor, []).
+
+process(Cursor, Acc) ->
+    case mongo:next(Cursor) of
+        {} ->
+            Acc;
+        {Result} ->
+            JResult = bson:fields(Result),
+            process(Cursor, [JResult|Acc])
+    end.
